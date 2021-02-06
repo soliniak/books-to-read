d6f629164d635e378ed00b2215e2bb3aa59df7c7
soliniak <soliniak@gmail.com>
2021-02-06 18:25:54 +0100

    Added check if user really want to delete book from list

--- src/deleteBook.js
+++ src/deleteBook.js
@@ -1,21 +1,31 @@
 import generateTable from './generateTable';
+import handleMessage from './handleMessage';
 
-const deleteBook = () => {
+const deleteBook = (index) => {
     const booksFromLoaclStorage = JSON.parse(localStorage.getItem('booksToRead'));
 
     const deleteBtns = document.querySelectorAll('.delete-book');
 
-    deleteBtns.forEach(( btn ) => {
-        btn.addEventListener('click', (e) => {
-            booksFromLoaclStorage.forEach((book, index) => {
-                if(book.id === Number(e.target.dataset.id)) {
-                    booksFromLoaclStorage.splice(index, 1);
-                    localStorage.setItem('booksToRead', JSON.stringify(booksFromLoaclStorage));
-                    generateTable();   
-                }
-            });        
+
+    if(index) {
+        booksFromLoaclStorage.splice(index, 1);
+        localStorage.setItem('booksToRead', JSON.stringify(booksFromLoaclStorage));
+        generateTable();          
+    } else {
+        deleteBtns.forEach(( btn ) => {
+            btn.addEventListener('click', (e) => {
+                booksFromLoaclStorage.forEach((book, index) => {
+                    if(book.id === Number(e.target.dataset.id)) {
+                        if(handleMessage(`Czy na pewno chcesz usunąć ${book.title}?`, index)) {
+                            booksFromLoaclStorage.splice(index, 1);
+                            localStorage.setItem('booksToRead', JSON.stringify(booksFromLoaclStorage));
+                            generateTable();   
+                        }
+                    }
+                });        
+            }, {once: true});
         });
-    });
+    }
 };
 
 export default deleteBook;
--- src/handleMessage.js
+++ src/handleMessage.js
@@ -1,14 +1,38 @@
-const handleMessage = (message) => {
+import deleteBook from './deleteBook';
+
+const handleMessage = (message, index) => {
     const messageContainer = document.querySelector('.modal-message');
     const messageDiv = messageContainer.querySelector('.message');
+    const messageConfirm = messageContainer.querySelector('.message-confirm');
 
+    messageConfirm.innerHTML = '';
     messageDiv.textContent = message;
     messageContainer.classList.add('modal-active');
 
-    setTimeout(() => {
-        messageContainer.classList.remove('modal-active');
-        // messageDiv.textContent = '';
-    }, 2000);
+    if(!Number(index)) {
+        setTimeout(() => {
+            messageContainer.classList.remove('modal-active');
+            // messageDiv.textContent = '';
+        }, 2000);
+    } else {
+        messageConfirm.innerHTML = `
+            <button class="btn-primary confirm">Tak</button>
+            <button class="btn-primary decline">Nie</button>
+        `;
+
+        const confirmBtn = document.querySelector('.confirm');
+        const declineBtn = document.querySelector('.decline');
+
+        confirmBtn.addEventListener('click', () => {
+            deleteBook(index);
+            index = '';
+            messageContainer.classList.remove('modal-active');
+        });
+
+        declineBtn.addEventListener('click', () => {
+            messageContainer.classList.remove('modal-active');
+        });
+    }
 };
 
 export default handleMessage;
